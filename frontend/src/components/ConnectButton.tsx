d38813dee84bce3c53b42ad73a8b1426462d2419
teefeh_07 <boluwatifeoniyilo@gmail.com>
2025-12-26 18:23:30 +0100

    feat: create connect button component

---
+++ frontend/src/components/ConnectButton.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { AppConfig, UserSession, showConnect } from '@stacks/connect';
+
+export const ConnectButton = () => {
+    return (
+        <button className="glass-button">
+            Connect Wallet
+        </button>
+    );
+};
